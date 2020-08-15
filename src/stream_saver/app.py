bf5165826e5289fac8e9823e35b9ee409bd0deb8
Zachary Joswick <joswick@torc.ai>
2020-08-15 12:55:06 -0400

    Adding ffmpeg, stub src and sink

--- src/stream_saver/app.py
+++ src/stream_saver/app.py
@@ -2,8 +2,18 @@
 A tool to automatically download streams live and avoid muted VODs
 """
 
+import os
+import code
+from os.path import dirname, abspath
+
 
 def main():
     # This should start and launch your app!
     print("Hello World")
-    pass
+    print(os.path.realpath(__file__))
+
+    print(f"Appdir is: {os.getenv('APPDIR', 'APPDIR not found')}")
+
+    d = dirname(abspath(__file__))
+    print(f"contents of directory: {os.listdir(d)}")
+    code.InteractiveConsole(locals=globals()).interact()
